8e48d6c5f9f7260534fa70820beb493a91f5a91b
naoto613 <fujii140@gmail.com>
2023-04-16 23:32:33 +0900

    一括更新機能の追加

--- lib/todo_phoenix/todo.ex
+++ lib/todo_phoenix/todo.ex
@@ -18,4 +18,34 @@ defmodule TodoPhoenix.Todo do
     |> Task.changeset(attrs)
     |> Repo.insert()
   end
+
+  def update_tables() do
+    target = "content"
+
+    File.stream!("test.csv")
+    |> Enum.reduce(Ecto.Multi.new(), fn line, multi ->
+      [id, value_str] = String.trim_trailing(line) |> String.split(",")
+      value = String.trim(value_str)
+
+      case Enum.find([Task], fn x -> target in Enum.map(x.__schema__(:fields), &to_string/1) end) do
+        Task ->
+          update_table(Task, id, target, value, multi)
+        nil ->
+          {:error, "Invalid target"}
+      end
+    end)
+    |> Repo.transaction()
+  end
+
+
+  defp update_table(table, id, target, value, multi) do
+    data = Repo.get(table, id)
+    changeset = table.changeset(data, Map.new([{target, value}]))
+
+    if String.length(value) <= 50 do
+      Ecto.Multi.update(multi, "task_#{id}", changeset)
+    else
+      multi
+    end
+  end
 end
